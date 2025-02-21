20128f963ac5fa64cae2373260a40463a1ff41c2
Dabhi Amit <amitdabhi526@gmail.com>
2025-02-21 14:04:06 +0530

    operator

---
+++ C++/02_variable/index.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 100;
+    int b = 200;
+
+    cout << "Sum of a & b =";
+    cout << a+b;
+
+    return 0;
+}
---
+++ C++/03_operator/index.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+
+    // int a = 10;
+    // int b = 20;
+
+
+    // relational operator
+
+    // a < b true
+    // a > b false
+    // a == b false
+    // a <= b true
+    // a >= b false
+    // a != b true
+
+
+
+
+
+
+    // logical operator
+
+    // a == b && a == b false
+    // a == b || a == b false
+    // a < b && b > a true
+    // a > b || b > a true
+    // a > b || b < a flase
+
+
+
+
+
+
+    // asignment operator
+
+    // int a = 100;
+    // int b = 80;
+
+    // b = a+b;
+    // b += a;
+    // b = a-b;
+    // b -= a;
+
+
+
+
+
+
+    // miscellaneous operator
+
+    // cout << sizeof(a) << endl;
+    // cout << sizeof(b) << endl;
+
+
+
+
+
+
+    // unary operator
+
+    // cout << a-- << endl;
+    // cout << a-- << endl;
+    // cout << --a << endl;
+    // cout << b++ << endl;
+    // cout << b++ << endl;
+    // cout << ++b << endl;
+
+
+
+
+
+
+    cout <<  1 + 4 * 2 - 3 / 3 + 2 * 2 / 4 << endl;
+
+    return 0;
+}
