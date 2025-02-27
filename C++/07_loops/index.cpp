7d658000ac850cfa7899c33d768b6c97142c1e25
Dabhi Amit <amitdabhi526@gmail.com>
2025-02-27 13:13:37 +0530

    while-loop

--- C++/07_loops/index.cpp
+++ C++/07_loops/index.cpp
@@ -16,16 +16,50 @@ int main(){
 
 
 
-    int i = 1;
-    int num;
-    cout << "Enter Number: ";
-    cin >> num;
+    // int i = 1;
+    // int num;
+    // cout << "Enter Number: ";
+    // cin >> num;
 
-    while (i <= 10)
-    {
-        cout << num << " x " << i << " = " << num*i << endl;
-        i++; 
-    }
+    // while (i <= 10)
+    // {
+    //     cout << num << " x " << i << " = " << num*i << endl;
+    //     i++; 
+    // }
+
+
+
+
+
+
+    // int x = 1;
+  
+    // while(x<5)
+    // {
+    //  cout << "table of 2" << x <<  endl;    
+    //  x = x*2;    
+    // }
+
+
+
+
+
+
+    //  int i =1;
+    //  int sum = 1;
+
+
+    //  while(i<=5)
+    //  {
+    
+    //     sum = sum+i;
+    
+    //     cout << i << " = " <<  endl;
+    
+    //      i++;
+    // }
+
+
 
     return 0;
 }
