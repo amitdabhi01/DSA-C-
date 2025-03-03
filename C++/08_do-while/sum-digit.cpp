c57e2e77835e73e43b6ce5d8fcc118f8e94c3b73
Dabhi Amit <amitdabhi526@gmail.com>
2025-03-03 13:27:05 +0530

    do-while

---
+++ C++/08_do-while/sum-digit.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int i = 67234;
+    int num_digit = 0;
+
+    while (i>0)
+    {
+        i = i/10;
+        num_digit++;
+    }
+
+    cout << num_digit << endl;
+    
+}
