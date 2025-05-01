429149b1ce033a8edbc9844071f24f40059d1e3a
Dabhi Amit <amitdabhi526@gmail.com>
2025-05-01 14:03:12 +0530

    vector

---
+++ C++/35_vector/index.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// template <typename T>
+
+// class vector1{
+//     public:
+//         T size;
+//         T arr[3];
+
+//         vector(){
+//             this->size = size;
+//             arr[this->size];  
+//         }
+
+//         void push_back(else){
+//             arr[0] = else;
+//         }
+// };
+
+
+int main(){
+
+    // vector1<int>x(3);
+    // x.push_back(10);
+
+    vector <int> arr(3, 12);
+
+    arr.push_back(13);
+    arr.push_back(14);
+
+    return 0;
+}
